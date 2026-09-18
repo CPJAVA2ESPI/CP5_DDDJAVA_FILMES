b612148c7329e9214bdc287a58addddb2fb9910c
Giovana Carnevali <giovanacarnevali@outlook.com>
2026-09-18 20:34:50 -0300

    Classe filme e ConnectionFactory

--- src/br/com/fiap/model/dao/ConnectionFactory.java
+++ src/br/com/fiap/model/dao/ConnectionFactory.java
@@ -1,4 +1,77 @@
 package br.com.fiap.model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//Giovana Carnevali -  RM566196
+//Henrique Vicente - RM564116
+//Yasmin Amorin - RM563645
+
 public class ConnectionFactory {
+
+    public static Connection abrirConexao() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            String url =
+                    "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+
+            final String USER = "rm566196";
+            final String PASS = "151206";
+
+            con = DriverManager.getConnection(
+                    url,
+                    USER,
+                    PASS
+            );
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println(
+                    "Erro: A classe de conexão não foi encontrada!\n"
+                            + e.getMessage()
+            );
+
+        } catch (SQLException e) {
+
+            System.out.println(
+                    "Erro de SQL!\n"
+                            + e.getMessage()
+            );
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Erro: " + e.getMessage()
+            );
+        }
+
+        return con;
+    }
+
+    public static void fecharConexao(Connection con) {
+
+        try {
+
+            con.close();
+
+        } catch (SQLException e) {
+
+            System.out.println(
+                    "Erro de SQL!\n"
+                            + e.getMessage()
+            );
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Erro: " + e.getMessage()
+            );
+        }
+    }
 }
--- src/br/com/fiap/model/dto/Filme.java
+++ src/br/com/fiap/model/dto/Filme.java
@@ -1,4 +1,48 @@
 package br.com.fiap.model.dto;
 
+//Giovana Carnevali -  RM566196
+//Henrique Vicente - RM564116
+//Yasmin Amorin - RM563645
+
 public class Filme {
+
+    private int codigo;
+    private String titulo;
+    private String genero;
+    private String produtora;
+
+    public Filme() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getProdutora() {
+        return produtora;
+    }
+
+    public void setProdutora(String produtora) {
+        this.produtora = produtora;
+    }
 }
